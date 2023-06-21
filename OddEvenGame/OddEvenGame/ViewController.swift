319879f5cccfa0b5d5553b5ef9da1cf51fa61742
AppLutter <tjrals604@naver.com>
2023-06-21 23:36:59 +0900

    create new project

---
+++ OddEvenGame/OddEvenGame/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  OddEvenGame
+//
+//  Created by seokmin on 2023/06/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
